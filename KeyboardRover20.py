8418400b3d8083075585785303eeabc17b92e016
josh <palmerjoshua2013@fau.edu>
2015-02-17 00:41:02 -0500

    basic WASD controls; movement lag possibly from batteries, but consider solutions

--- KeyboardRover20.py
+++ KeyboardRover20.py
@@ -5,22 +5,33 @@ from rover import Rover20
 
 MIN_BUTTON_LAG_SEC = 0.25
 
-class keyboardrover20(Rover20):
+# vals < 1 yield slower speed
+MAX_TREAD_SPEED = 0.5
+
+class KeyboardRover20(Rover20):
 	def __init__(self):
 		Rover20.__init__(self)
 		
 		self.quit = False
+		
+		'''
+		Trying to prevent repeated instructions that may be causing lag,
+		but lag could be from another issue entirely (possibly batteries)
+		'''
 		self.lastbuttontime = 0
 		self.lastmovement = None
 		
+		# WASD controls
 		self.movements = [pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d]
 		
+		# window must be open and in focus for pygame to take input
 		self.windowSize = [640, 480]
 		
 		pygame.init()
 		pygame.display.set_caption("Keyboard Rover")
 		self.screen = pygame.display.set_mode(self.windowSize)
-		print "YES"
+		
+		# Ctrl-C in Terminal (not game window) quits program for now
 		try:
 			self.keyListen()
 		except KeyboardInterrupt:
@@ -32,6 +43,7 @@ class keyboardrover20(Rover20):
 			for event in pygame.event.get():
 				if event.type == KEYDOWN:
 					if event.key in self.movements and self.goodCommand(event.key):
+						
 						self.lastbuttontime = time.time()
 						self.lastmovement = event.key
 						
@@ -42,45 +54,51 @@ class keyboardrover20(Rover20):
 						elif event.key == self.movements[2]:
 							self.moveBackward()
 						elif event.key == self.movements[3]:
-							self.turnRight()
-						elif event.key == pygame.K_q:
-							self.halt()
-							listening = False					
+							self.turnRight()				
 						else:
-							pass												
+							#ignore other keys for now
+							pass
+																		
 				if event.type == KEYUP:
+					# letting go of WASD stops movement	
 					if event.key in self.movements:
 						self.halt()
+				
 				if event.type == QUIT:
 					listening = False
 		self.quit = True	
-		
+
 	def moveForward(self):
-		self.setTreads(0.5,0.5)
+		self.setTreads(MAX_TREAD_SPEED, MAX_TREAD_SPEED)
 	
 	def moveBackward(self):
-		self.setTreads(-0.5,-0.5)
+		self.setTreads(-MAX_TREAD_SPEED, -MAX_TREAD_SPEED)
 		
 	def turnRight(self):
-		self.setTreads(0.5,0)
+		self.setTreads(MAX_TREAD_SPEED,0)
 		
 	def turnLeft(self):
-		self.setTreads(0,0.5)
+		self.setTreads(0,MAX_TREAD_SPEED)
 	
 	def halt(self):
 		self.setTreads(0,0)
 	
+	'''
+	goodCommand() needs better function name	
+	prevents 'button jump'
+	'''
 	def goodCommand(self, key):
-		return ((time.time() - self.lastbuttontime) > MIN_BUTTON_LAG_SEC) and key is not self.lastmovement
+		return ((time.time() - self.lastbuttontime) > MIN_BUTTON_LAG_SEC) and (key is not self.lastmovement)
 	
 def main():	
-	rover = keyboardrover20()
+	rover = KeyboardRover20()
 	
 	while not rover.quit:
 		pass
+	
 	rover.close()
 	pygame.quit()
-	print "DONE"
+
 			
 if __name__ == '__main__':
 	main()	
